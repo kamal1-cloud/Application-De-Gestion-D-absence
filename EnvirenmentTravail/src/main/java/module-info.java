fd9bce5c3fb12e1fefe5f6343f25c5c0f5316180
kamal habrich <kamalhabrich50@gmail.com>
2021-01-15 10:24:11 +0100

    Ajouter L'envirenement de Travail

--- EnvirenmentTravail/src/main/java/module-info.java
+++ EnvirenmentTravail/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module ma.youcode.GestionDabsence {
     requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens ma.youcode.GestionDabsence to javafx.fxml;
     exports ma.youcode.GestionDabsence;
 }
