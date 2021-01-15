554591218b1d350a9365bc2f378891ff23442826
kamal habrich <kamalhabrich50@gmail.com>
2021-01-15 15:29:30 +0100

    La connection avec la base de donner
    Ajouter ce script d'établir la connexion avec la base de données pour eviter les conflets

--- EnvirenmentTravail/src/main/java/ma/youcode/GestionDabsence/Connectivity/DbConnection.java
+++ EnvirenmentTravail/src/main/java/ma/youcode/GestionDabsence/Connectivity/DbConnection.java
@@ -1,4 +1,15 @@
 package ma.youcode.GestionDabsence.Connectivity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestiondesetudiants","root","");
+        System.out.println("Connection etablie");
+        return conn;
+}
+
 }
--- EnvirenmentTravail/src/main/java/module-info.java
+++ EnvirenmentTravail/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ma.youcode.GestionDabsence {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
     opens ma.youcode.GestionDabsence to javafx.fxml;
     exports ma.youcode.GestionDabsence;
