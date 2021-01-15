a193af0f619d2224470b1354ab6d7c46073527a2
kamal habrich <kamalhabrich50@gmail.com>
2021-01-15 11:10:07 +0100

    Mise a jour l'environment de travail

--- EnvirenmentTravail/src/main/java/module-info.java
+++ EnvirenmentTravail/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module ma.youcode.GestionDabsence {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
 
     opens ma.youcode.GestionDabsence to javafx.fxml;
     exports ma.youcode.GestionDabsence;
