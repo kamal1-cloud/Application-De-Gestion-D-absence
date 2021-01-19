7dbc92962a580bcd4d3ddcc829f3edd5384c049e
Kamal Habrich <kamalhabrich50@gmail.com>
2021-01-19 16:39:33 +0100

    Add Static

--- EnvirenmentTravail/src/main/java/ma/youcode/GestionDabsence/Connectivity/DbConnection.java
+++ EnvirenmentTravail/src/main/java/ma/youcode/GestionDabsence/Connectivity/DbConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DbConnection {
     //TODO  changer le nom de la base de donner avec le nom de nous base de donner (jai juste tester la connection avec ce nom (gestiondesetudiants))
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection conn;
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionDabsence","root","");
         System.out.println("Connection etablie");
